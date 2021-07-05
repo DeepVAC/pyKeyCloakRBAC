ccd1769c8a1e64b3b8a2e2e7afcc01dc7388168b
forths <wangashaoyang@163.com>
2021-07-05 19:21:42 +0800

    add restapi demo and update some admin operations

---
+++ demos/admindemo.py
@@ -0,0 +1,62 @@
+from pyKeyCloakRBAC.pyrbac import PyRBACAdmin
+
+if __name__ == '__main__':
+    server_url = "http://xxxx:xxxx/auth/"
+    # 管理员操作
+    username = "realmadmin"
+    password = "1111"
+    client_id = "admin-cli"
+    realmname = "demo"
+    pa = PyRBACAdmin(server_url, username, password,
+                     realm_name=realmname, verify=False)
+    # 角色操作
+    rolename = "svip"
+    print("获取所有角色：")
+    realm_roles = pa.get_all_roles()
+    print("\t", realm_roles)
+    print("新增角色 {}：".format(rolename))
+    res = pa.create_role(rolename)
+    print("\t", res)
+    print("获取角色 {} ID: ".format(rolename))
+    print("\t", pa.get_role_id(rolename))
+    print("获取 role based policy：")
+    print("\t", pa.get_client_policies("test_api"))
+    # 更新角色信息
+    # print(pa.update_role(rolename, "newsvip"))
+    # 删除角色,当有角色和用户绑定时，默认不删除
+    # print("删除角色 delete_role:")
+    # print(pa.delete_role(rolename))
+    # 用户操作
+    username = "Alice"
+    password = "abcdefg"
+    print("新增用户 {}:".format(username))
+    payload = {"enabled": True, "emailVerified": "", "username": username,
+               "credentials": [{"type": "password", "value": password, "temporary": False}]}
+    try:
+        uid = pa.create_user(payload, exist_ok=False)
+        print("\t", uid)
+    except Exception as e:
+        print("\t", str(e))
+    print("获取用户 {} id：".format(username))
+    print("\t", pa.get_user_id(username))
+    # 操作用户和角色
+    print("分配给用户角色：")
+    print("\t", pa.assign_role_to_user(username, rolename))
+    print("删除用户角色:")
+    print("\t", pa.delete_realm_user_role(username, rolename))
+    print("获取角色 {} 下的所有用户：".format(rolename))
+    print("\t", pa.get_realm_role_members(rolename))
+    # 资源操作,同时初始化对应permission
+    resource_client = "test_api"
+    resource = "/api/v1/forall"
+    try:
+        print("创建资源: ")
+        print("\t", pa.create_resource(resource_client, resource))
+        print('\t', pa.create_client_permission(resource_client, resource))
+    except Exception as e:
+        print("\t", str(e))
+    print("获取resource_client {}的所有api：".format(resource_client))
+    print("\t", pa.get_resources(resource_client))
+    # 权限操作,权限基于资源，可以绑定角色策略。
+
+
--- pyKeyCloakRBAC/pyrbac.py
+++ pyKeyCloakRBAC/pyrbac.py
@@ -4,6 +4,8 @@ from .exceptions import raise_error_from_response, KeycloakGetError
 from jose import jwt
 from .urls_patterns import (
     URL_TOKEN,
+    URL_ADMIN_USER_REALM_ROLES,
+    URL_ADMIN_CLIENT_AUTHZ_RESOURCES
 )
 import json
 
@@ -11,55 +13,25 @@ class PyRBACAdmin(KeycloakAdmin):
     def __init__(self, server_url, username, password, realm_name="master", client_id="admin-cli", verify=True, client_secret_key=None, custom_headers=None, user_realm_name=None, auto_refresh_token=None):
         super().__init__(server_url, username=username, password=password, realm_name=realm_name, client_id=client_id, verify=verify,
                          client_secret_key=client_secret_key, custom_headers=custom_headers, user_realm_name=user_realm_name, auto_refresh_token=auto_refresh_token)
-#     def init_permission(self, permissions):
-#         # permissions 权限列表
-#         # 为client定义角色，代表API权限,clientid为admin-cli
-#         self.get_admin_cid()
-#         srcp = self.get_all_permissions()
-#         if not srcp:
-#             toadd = permissions
-#         else:
-#             toadd = list(set(permissions) - set(srcp.keys()))
-#         if not toadd:
-#             return self.errormsg("permissions {} already exist.".format(permissions))
-#         for permission in toadd:
-#             self.add_permission(permission)
 
-#     def get_all_permissions(self):
-#         # 获取所有已定义权限
-#         self.get_admin_cid()
-#         return {key["name"]: key["id"] for key in self.get_client_roles(self.cid)}
+    def create_resource(self, client_id, resource, displayName="default"):
+        payload = {"uris":[resource],"name":resource,"displayName":displayName}
+        cid = self.get_client_id(client_id)
+        params_path = {"realm-name": self.realm_name, "id": cid}
+        data_raw = self.raw_post(URL_ADMIN_CLIENT_AUTHZ_RESOURCES.format(**params_path),
+                                 data=json.dumps(payload))
+        return raise_error_from_response(data_raw, KeycloakGetError, expected_codes=[201])
 
-#     def add_permission(self, name):
-#         # 添加单个权限
-#         permissions = self.get_all_permissions()
-#         if name in permissions:
-#             return self.errormsg("Roles {} already exist.".format(name))
-#         url = "admin/realms/{realm-name}/clients/{cid}/roles"
-#         params_path = {"realm-name": self.realm_name, "cid": self.cid}
-#         data_raw = self.raw_post(url.format(**params_path),
-#                                  data=json.dumps({"name": name}))
-#         return raise_error_from_response(data_raw, KeycloakGetError, expected_code=201)
+    def get_resources(self, client_id):
+        cid = self.get_client_id(client_id)
+        return [item['name'] for item in self.get_client_authz_resources(cid)]
 
-#     def delete_permission(self, rolename):
-#         # 删除单个权限
-#         valid = self.get_all_permissions()
-#         if rolename in valid:
-#             crid = valid[rolename]
-#         else:
-#             return self.errormsg("get no permission called {}".format(rolename))
-#         url = "admin/realms/{realm-name}/roles-by-id/{role-id}"
-#         params_path = {"realm-name": self.realm_name, "role-id": crid}
-#         data_raw = self.raw_delete(url.format(**params_path))
-#         return raise_error_from_response(data_raw, KeycloakGetError, expected_code=204)
-
-#     def delete_org(self, orgname):
-#         # 删除组织
-#         groups = {key["name"]: key["id"] for key in self.get_groups(
-#         ) if key["name"].split("_")[0] == orgname}
-#         print(groups)
-#         for rid in groups.values():
-#             self.delete_group(rid)
+    def get_resource_id(self, client_id, resourcename):
+        resources = self.get_client_authz_resources(self.get_client_id(client_id))
+        print(resources)
+        for resource in resources:
+            if resource['name'] == resourcename:
+                return resource['_id']
 
     def get_all_roles(self):
         return {key["name"]: key["id"] for key in self.get_realm_roles()}
@@ -68,8 +40,24 @@ class PyRBACAdmin(KeycloakAdmin):
         for role in self.get_realm_roles():
             if rolename == role["name"]:
                 return role["id"]
+    
+    def create_client_policy_with_role(self, client_id, policyname, rolename):
+        rid = self.get_role_id(rolename)
+        payload ={"type":"role","logic":"POSITIVE","decisionStrategy":"UNANIMOUS","name":policyname,"roles":[{"id":rid}]}
+        cid = self.get_client_id(client_id)
+        url = "admin/realms/{realm-name}/clients/{cid}/authz/resource-server/policy/role"
+        params_path = {"realm-name": self.realm_name, "cid": cid}
+        data_raw = self.raw_post(url.format(**params_path),
+                                 data=json.dumps(payload))
+        return raise_error_from_response(data_raw, KeycloakGetError)
 
-    def add_role(self, rolename):
+    def get_client_policies(self, client_id):
+        url = "admin/realms/{realm-name}/clients/{cid}/authz/resource-server/policy"
+        params_path = {"realm-name": self.realm_name, "cid": self.get_client_id(client_id)}
+        data_raw = self.raw_get(url.format(**params_path))
+        return raise_error_from_response(data_raw, KeycloakGetError)
+
+    def create_role(self, rolename):
         # 新增角色
         try:
             return self.create_realm_role({"name": rolename})
@@ -97,6 +85,14 @@ class PyRBACAdmin(KeycloakAdmin):
             return self.errormsg("rolename {} alread exists".format(rolename))
         self.update_realm_role(srcname, {"name": rolename})
 
+    def create_client_permission(self, client_id, resource):
+        cid = self.get_client_id(client_id)
+        url = "admin/realms/{realm-name}/clients/{cid}/authz/resource-server/permission/resource"
+        payload = {"type":"resource","logic":"POSITIVE","decisionStrategy":"UNANIMOUS","name":resource,"resources":[self.get_resource_id(client_id, resource)],"policies":[]}
+        params_path = {"realm-name": self.realm_name, "cid": cid}
+        data_raw = self.raw_post(url.format(**params_path), data=json.dumps(payload))
+        return raise_error_from_response(data_raw, KeycloakGetError)
+    
 #     def get_role_permissions(self, rolename):
 #         rid = self.get_role_id(rolename)
 #         if not rid:
@@ -108,27 +104,27 @@ class PyRBACAdmin(KeycloakAdmin):
 #         data_raw = self.raw_get(url.format(**params_path))
 #         return raise_error_from_response(data_raw, KeycloakGetError, expected_code=200)
 
-#     def assign_permissions_to_role(self, rolename, permissions):
-#         rid = self.get_role_id(rolename)
-#         if not rid:
-#             return self.errormsg("role {} not found".format(rolename))
-#         allp = self.get_all_permissions()
-#         if not allp:
-#             return self.errormsg("no permission now,need to init permissions")
-#         invalid = list(set(permissions) - set(allp.keys()))
-#         if invalid:
-#             return self.errormsg("permission {} not in current permissions".format(invalid))
-#         srcpermissions = self.get_role_permissions(rolename)
-#         srcp = [name["name"] for name in srcpermissions]
-#         self.delete_permissions_from_role(rolename, srcp)
-#         url = "admin/realms/{realm-name}/groups/{rid}/role-mappings/clients/{cid}"
-#         params_path = {"realm-name": self.realm_name,
-#                        "cid": self.cid, "rid": rid}
-#         payload = [{"id": allp[name], "name": name, "containerId": self.cid}
-#                    for name in permissions]
-#         data_raw = self.raw_post(url.format(**params_path),
-#                                  data=json.dumps(payload))
-#         return raise_error_from_response(data_raw, KeycloakGetError, expected_code=204)
+    def assign_permissions_to_role(self, rolename, permissions):
+        rid = self.get_role_id(rolename)
+        if not rid:
+            return self.errormsg("role {} not found".format(rolename))
+        allp = self.get_all_permissions()
+        if not allp:
+            return self.errormsg("no permission now,need to init permissions")
+        invalid = list(set(permissions) - set(allp.keys()))
+        if invalid:
+            return self.errormsg("permission {} not in current permissions".format(invalid))
+        srcpermissions = self.get_role_permissions(rolename)
+        srcp = [name["name"] for name in srcpermissions]
+        self.delete_permissions_from_role(rolename, srcp)
+        url = "admin/realms/{realm-name}/groups/{rid}/role-mappings/clients/{cid}"
+        params_path = {"realm-name": self.realm_name,
+                       "cid": self.cid, "rid": rid}
+        payload = [{"id": allp[name], "name": name, "containerId": self.cid}
+                   for name in permissions]
+        data_raw = self.raw_post(url.format(**params_path),
+                                 data=json.dumps(payload))
+        return raise_error_from_response(data_raw, KeycloakGetError, expected_code=204)
 
 #     def delete_permissions_from_role(self, rolename, permissions):
 #         rid = self.get_role_id(rolename)
@@ -142,7 +138,7 @@ class PyRBACAdmin(KeycloakAdmin):
 #                        "cid": self.cid, "rid": rid}
 #         data_raw = self.raw_delete(url.format(**params_path),
 #                                    data=json.dumps(toremove))
-#         return raise_error_from_response(data_raw, KeycloakGetError, expected_code=204)
+#         return raise_error_from_response(data_raw, KeycloakGetError)
 
     def assign_role_to_user(self, username, rolename):
         uid = self.get_user_id(username)
@@ -150,18 +146,18 @@ class PyRBACAdmin(KeycloakAdmin):
             return self.errormsg("username {} not found".format(username))
         return self.assign_realm_roles(uid, self.get_realm_role(rolename))
 
-    # def delete_realm_user_role(self, username, rolename):
-    #     uid = self.get_user_id(username)
-    #     if not uid:
-    #         return self.errormsg("username {} not found".format(username))
-    #     rid = self.get_role_id(rolename)
-    #     if not rid:
-    #         return self.errormsg("role {} not found".format(rolename))
-    #     url = "admin/realms/{realm-name}/users/{uid}/role-mappings/realm"
-    #     params_path = {"realm-name": self.realm_name, "uid": uid}
-    #     data_raw = self.raw_delete(url.format(**params_path), data=self.get_realm_role(rolename))
-    #     print(data_raw)
-    #     return raise_error_from_response(data_raw, KeycloakGetError, expected_codes=204)
+    def delete_realm_user_role(self, username, rolename):
+        uid = self.get_user_id(username)
+        if not uid:
+            return self.errormsg("username {} not found".format(username))
+        rid = self.get_role_id(rolename)
+        if not rid:
+            return self.errormsg("role {} not found".format(rolename))
+        data = [self.get_realm_role(rolename)]
+        params_path = {"realm-name": self.realm_name, "id": uid}
+        data_raw = self.raw_delete(URL_ADMIN_USER_REALM_ROLES.format(**params_path),
+                                 data=json.dumps(data))
+        return raise_error_from_response(data_raw, KeycloakGetError)
 
     def errormsg(self, msg):
         return {"error": msg}
